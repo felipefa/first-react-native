0cdb8f1506fa7067bf5e513122622a67e85ca498
Felipe Araujo <devaraujofelipe@gmail.com>
2019-10-08 22:29:11 -0300

    Consume Github API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,61 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCaptalize="none"
-          autoCorrect={false}
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#fff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  constructor() {
+    super().state = {
+      newUser: '',
+      users: [],
+    };
+  }
+
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    const { data } = await api.get(`/users/${newUser}`);
+
+    const user = {
+      name: data.name,
+      login: data.login,
+      bio: data.bio,
+      avatar: data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, user],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { newUser, users } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCaptalize="none"
+            autoCorrect={false}
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
